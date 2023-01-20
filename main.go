a9b5207c0cfaf0af32ca29826f14a93d6fe1ad03
gorisuke <sota.naoi@juge6.jp>
2023-01-20 23:16:18 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+	ua := ""
+	// ミドルウェアを使用
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		fmt.Println("Use Middle Ware")
+		c.Next()
+	})
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":    "hello world",
+			"User-Agent": ua,
+		})
+	})
+	engine.Run(":3000")
+}
