6ac271f53e0ae266e46ddc42a14812cc86aa51cf
Sota <83456804+gorisuke@users.noreply.github.com>
2023-09-03 10:37:01 +0900

    routerに移行

--- main.go
+++ main.go
@@ -2,73 +2,18 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"time"
-	"umi/auth"
+	"umi/router"
 
-	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
 )
 
-var serverStartedTime = time.Now()
-
 func main() {
-	color.Yellow("Starting....")
+	log.Println("ServerStarting...")
 	engine := gin.Default()
-	ua := ""
-
-	color.Green("Started Server")
 	engine.Use(func(c *gin.Context) {
-		ua = c.GetHeader("User-Agent")
 		log.Println("All Request Middleware")
 		c.Next()
 	})
-	engine.GET("/", func(c *gin.Context) {
-		time := time.Now()
-		id := c.Query("id")
-		c.JSON(http.StatusOK, gin.H{
-			"message":    "This API is Test API!!!!!!!!!!!",
-			"User-Agent": ua,
-			"accessTime": time,
-			"requestId":  id,
-		})
-	})
-
-	engine.POST("/", func(c *gin.Context) {
-		time := time.Now()
-		name := c.PostForm("name")
-		age := c.PostForm("age")
-		c.JSON(http.StatusOK, gin.H{
-			"message":    "hello world",
-			"accessTime": time,
-			"name":       name,
-			"age":        age,
-		})
-	})
-
-	engine.GET("/user", func(c *gin.Context) {
-		id := c.Query("id")
-		c.JSON(http.StatusOK, gin.H{
-			"message":    "hello world",
-			"User-Agent": ua,
-			"id":         id,
-		})
-	})
-
-	engine.GET("/schedule", protectedMiddleware(), func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.SetRoutes(engine)
 	engine.Run(":3000")
-}
-func protectedMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		log.Println(auth.AuthUmi(c))
-		if c.Query("id") != "" {
-			c.Next()
-		} else {
-			c.AbortWithStatus(403)
-		}
-	}
 }
---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetRoutes(engine *gin.Engine) {
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "This API is Test API!!!!!!!!!!!",
+		})
+	})
+}
